efc1669b3e8e62dbbcebd642f15e9200fda2d13f
GyDi <segydi@foxmail.com>
2021-12-08 23:34:23 +0800

    feat: use vite

---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  root: "src",
+  plugins: [react()],
+});
