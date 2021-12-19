2a7cabb06dd9e41021bf75f61a28e48c3a935476
GyDi <segydi@foxmail.com>
2021-12-19 17:48:38 +0800

    fix: proxy bypass add <local>

--- src-tauri/src/cmds/some.rs
+++ src-tauri/src/cmds/some.rs
@@ -48,7 +48,7 @@ pub fn set_sys_proxy(enable: bool, clash_info: State<'_, ClashInfoState>) -> Res
   let config = if enable {
     let server = format!("127.0.0.1:{}", port.unwrap());
     // todo
-    let bypass = String::from("localhost;127.*;10.*;172.16.*;172.17.*;172.18.*;172.19.*;172.20.*;172.21.*;172.22.*;172.23.*;172.24.*;172.25.*;172.26.*;172.27.*;172.28.*;172.29.*;172.30.*;172.31.*;192.168.*");
+    let bypass = String::from("localhost;127.*;10.*;172.16.*;172.17.*;172.18.*;172.19.*;172.20.*;172.21.*;172.22.*;172.23.*;172.24.*;172.25.*;172.26.*;172.27.*;172.28.*;172.29.*;172.30.*;172.31.*;192.168.*;<local>");
 
     SysProxyConfig {
       enable,
