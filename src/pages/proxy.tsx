df158a741a3cf4ba475265d736f52f88e486637d
GyDi <segydi@foxmail.com>
2022-01-10 21:25:41 +0800

    fix: get proxies multiple times

--- src/pages/proxy.tsx
+++ src/pages/proxy.tsx
@@ -1,11 +1,21 @@
-import useSWR from "swr";
+import useSWR, { useSWRConfig } from "swr";
+import { useEffect } from "react";
 import { Box, List, Paper, Typography } from "@mui/material";
 import { getProxies } from "../services/api";
 import ProxyGroup from "../components/proxy-group";
 
 const ProxyPage = () => {
+  const { mutate } = useSWRConfig();
   const { data: proxiesData } = useSWR("getProxies", getProxies);
   const { groups = [] } = proxiesData ?? {};
+
+  useEffect(() => {
+    // fix the empty proxies on the first sight
+    // this bud only show on the build version
+    // call twice to avoid something unknown or the delay of the clash startup
+    setTimeout(() => mutate("getProxies"), 250);
+    setTimeout(() => mutate("getProxies"), 1000);
+  }, []);
 
   return (
     <Box sx={{ width: 0.9, maxWidth: "850px", mx: "auto", mb: 2 }}>
