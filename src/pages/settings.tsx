40977785c3d3ca1c4360ffdc592e8a331556319c
GyDi <segydi@foxmail.com>
2022-01-17 02:42:52 +0800

    feat: adjust setting typography

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -2,16 +2,26 @@ import { Paper } from "@mui/material";
 import BasePage from "../components/base-page";
 import SettingVerge from "../components/setting/setting-verge";
 import SettingClash from "../components/setting/setting-clash";
+import SettingSystem from "../components/setting/setting-system";
+import Notice from "../components/notice";
 
 const SettingPage = () => {
+  const onError = (error: any) => {
+    error && Notice.error(error.toString());
+  };
+
   return (
     <BasePage title="Settings">
-      <Paper sx={{ borderRadius: 1, boxShadow: 2 }}>
-        <SettingVerge />
+      <Paper sx={{ borderRadius: 1, boxShadow: 2, mb: 3 }}>
+        <SettingClash onError={onError} />
       </Paper>
 
-      <Paper sx={{ borderRadius: 1, boxShadow: 2, mt: 3 }}>
-        <SettingClash />
+      <Paper sx={{ borderRadius: 1, boxShadow: 2, mb: 3 }}>
+        <SettingSystem onError={onError} />
+      </Paper>
+
+      <Paper sx={{ borderRadius: 1, boxShadow: 2 }}>
+        <SettingVerge onError={onError} />
       </Paper>
     </BasePage>
   );
