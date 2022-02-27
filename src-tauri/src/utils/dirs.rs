38778c8cac9bf5d6547de61a794e63ea7fad04bd
GyDi <segydi@foxmail.com>
2022-02-28 01:34:25 +0800

    refactor: use anyhow to handle error

--- src-tauri/src/utils/dirs.rs
+++ src-tauri/src/utils/dirs.rs
@@ -1,3 +1,4 @@
+use std::env::temp_dir;
 use std::path::{Path, PathBuf};
 use tauri::{
   api::path::{home_dir, resource_dir},
@@ -17,4 +18,35 @@ pub fn app_resources_dir(package_info: &PackageInfo) -> PathBuf {
   resource_dir(package_info, &Env::default())
     .unwrap()
     .join("resources")
+}
+
+/// profiles dir
+pub fn app_profiles_dir() -> PathBuf {
+  app_home_dir().join("profiles")
+}
+
+/// logs dir
+pub fn app_logs_dir() -> PathBuf {
+  app_home_dir().join("logs")
+}
+
+static CLASH_CONFIG: &str = "config.yaml";
+static VERGE_CONFIG: &str = "verge.yaml";
+static PROFILE_YAML: &str = "profiles.yaml";
+static PROFILE_TEMP: &str = "clash-verge-runtime.yaml";
+
+pub fn clash_path() -> PathBuf {
+  app_home_dir().join(CLASH_CONFIG)
+}
+
+pub fn verge_path() -> PathBuf {
+  app_home_dir().join(VERGE_CONFIG)
+}
+
+pub fn profiles_path() -> PathBuf {
+  app_home_dir().join(PROFILE_YAML)
+}
+
+pub fn profiles_temp_path() -> PathBuf {
+  temp_dir().join(PROFILE_TEMP)
 }
