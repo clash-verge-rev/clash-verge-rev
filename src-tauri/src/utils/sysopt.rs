c65f3c7b682f6413b0b264a3c2dc3bf193e63394
GyDi <segydi@foxmail.com>
2022-03-08 00:39:09 +0800

    chore: add default bypass

--- src-tauri/src/utils/sysopt.rs
+++ src-tauri/src/utils/sysopt.rs
@@ -3,6 +3,8 @@ use std::io;
 
 #[cfg(target_os = "windows")]
 static DEFAULT_BYPASS: &str = "localhost;127.*;10.*;172.16.*;172.17.*;172.18.*;172.19.*;172.20.*;172.21.*;172.22.*;172.23.*;172.24.*;172.25.*;172.26.*;172.27.*;172.28.*;172.29.*;172.30.*;172.31.*;192.168.*;<local>";
+#[cfg(target_os = "linux")]
+static DEFAULT_BYPASS: &str = "localhost,127.0.0.1/8,::1";
 #[cfg(target_os = "macos")]
 static DEFAULT_BYPASS: &str =
   "192.168.0.0/16\n10.0.0.0/8\n172.16.0.0/12\n127.0.0.1\nlocalhost\n*.local\ntimestamp.apple.com\n";
