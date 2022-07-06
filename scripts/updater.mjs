5e8dfe7267dcb780bd7eb028b65276321ddba596
GyDi <segydi@foxmail.com>
2022-07-07 02:54:20 +0800

    chore: update aarch rule

--- scripts/updater.mjs
+++ scripts/updater.mjs
@@ -83,11 +83,11 @@ async function resolveUpdater() {
     }
 
     // darwin url (aarch)
-    if (name.endsWith("aarch.app.tar.gz")) {
+    if (name.endsWith("aarch64.app.tar.gz")) {
       updateData.platforms["darwin-aarch64"].url = browser_download_url;
     }
     // darwin signature (aarch)
-    if (name.endsWith("aarch.app.tar.gz.sig")) {
+    if (name.endsWith("aarch64.app.tar.gz.sig")) {
       const sig = await getSignature(browser_download_url);
       updateData.platforms["darwin-aarch64"].signature = sig;
     }
