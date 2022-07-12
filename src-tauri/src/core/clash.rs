fbb17a0ba5d10bc9bd144344fe2e3e5cd1613ee9
limsanity <798607646@qq.com>
2022-07-13 00:43:27 +0800

    feat(system tray): support switch rule/global/direct/script mode in system tray

--- src-tauri/src/core/clash.rs
+++ src-tauri/src/core/clash.rs
@@ -5,285 +5,304 @@ use serde_yaml::{Mapping, Value};
 
 #[derive(Default, Debug, Clone, Deserialize, Serialize)]
 pub struct ClashInfo {
-  /// clash sidecar status
-  pub status: String,
+    /// clash sidecar status
+    pub status: String,
 
-  /// clash core port
-  pub port: Option<String>,
+    /// clash core port
+    pub port: Option<String>,
 
-  /// same as `external-controller`
-  pub server: Option<String>,
+    /// same as `external-controller`
+    pub server: Option<String>,
 
-  /// clash secret
-  pub secret: Option<String>,
+    /// clash secret
+    pub secret: Option<String>,
+
+    /// mode
+    pub mode: Option<String>,
 }
 
 impl ClashInfo {
-  /// parse the clash's config.yaml
-  /// get some information
-  pub fn from(config: &Mapping) -> ClashInfo {
-    let key_port_1 = Value::from("port");
-    let key_port_2 = Value::from("mixed-port");
-    let key_server = Value::from("external-controller");
-    let key_secret = Value::from("secret");
+    /// parse the clash's config.yaml
+    /// get some information
+    pub fn from(config: &Mapping) -> ClashInfo {
+        let key_port_1 = Value::from("port");
+        let key_port_2 = Value::from("mixed-port");
+        let key_server = Value::from("external-controller");
+        let key_secret = Value::from("secret");
+        let key_mode = Value::from("mode");
 
-    let port = match config.get(&key_port_1) {
-      Some(value) => match value {
-        Value::String(val_str) => Some(val_str.clone()),
-        Value::Number(val_num) => Some(val_num.to_string()),
-        _ => None,
-      },
-      _ => None,
-    };
-    let port = match port {
-      Some(_) => port,
-      None => match config.get(&key_port_2) {
-        Some(value) => match value {
-          Value::String(val_str) => Some(val_str.clone()),
-          Value::Number(val_num) => Some(val_num.to_string()),
-          _ => None,
-        },
-        _ => None,
-      },
-    };
+        let port = match config.get(&key_port_1) {
+            Some(value) => match value {
+                Value::String(val_str) => Some(val_str.clone()),
+                Value::Number(val_num) => Some(val_num.to_string()),
+                _ => None,
+            },
+            _ => None,
+        };
+        let port = match port {
+            Some(_) => port,
+            None => match config.get(&key_port_2) {
+                Some(value) => match value {
+                    Value::String(val_str) => Some(val_str.clone()),
+                    Value::Number(val_num) => Some(val_num.to_string()),
+                    _ => None,
+                },
+                _ => None,
+            },
+        };
 
-    // `external-controller` could be
-    // "127.0.0.1:9090" or ":9090"
-    let server = match config.get(&key_server) {
-      Some(value) => {
-        let val_str = value.as_str().unwrap_or("");
+        // `external-controller` could be
+        // "127.0.0.1:9090" or ":9090"
+        let server = match config.get(&key_server) {
+            Some(value) => {
+                let val_str = value.as_str().unwrap_or("");
 
-        if val_str.starts_with(":") {
-          Some(format!("127.0.0.1{val_str}"))
-        } else {
-          Some(val_str.into())
+                if val_str.starts_with(":") {
+                    Some(format!("127.0.0.1{val_str}"))
+                } else {
+                    Some(val_str.into())
+                }
+            }
+            _ => None,
+        };
+
+        let secret = match config.get(&key_secret) {
+            Some(value) => match value {
+                Value::String(val_str) => Some(val_str.clone()),
+                Value::Bool(val_bool) => Some(val_bool.to_string()),
+                Value::Number(val_num) => Some(val_num.to_string()),
+                _ => None,
+            },
+            _ => None,
+        };
+
+        let mode = match config.get(&key_mode) {
+            Some(value) => match value {
+                Value::String(val_str) => Some(val_str.clone()),
+                _ => None,
+            },
+            _ => None,
+        };
+
+        ClashInfo {
+            status: "init".into(),
+            port,
+            server,
+            secret,
+            mode,
         }
-      }
-      _ => None,
-    };
-
-    let secret = match config.get(&key_secret) {
-      Some(value) => match value {
-        Value::String(val_str) => Some(val_str.clone()),
-        Value::Bool(val_bool) => Some(val_bool.to_string()),
-        Value::Number(val_num) => Some(val_num.to_string()),
-        _ => None,
-      },
-      _ => None,
-    };
-
-    ClashInfo {
-      status: "init".into(),
-      port,
-      server,
-      secret,
     }
-  }
 }
 
 pub struct Clash {
-  /// maintain the clash config
-  pub config: Mapping,
+    /// maintain the clash config
+    pub config: Mapping,
 
-  /// some info
-  pub info: ClashInfo,
+    /// some info
+    pub info: ClashInfo,
 }
 
 impl Clash {
-  pub fn new() -> Clash {
-    let config = Clash::read_config();
-    let info = ClashInfo::from(&config);
+    pub fn new() -> Clash {
+        let config = Clash::read_config();
+        let info = ClashInfo::from(&config);
 
-    Clash { config, info }
-  }
-
-  /// get clash config
-  pub fn read_config() -> Mapping {
-    config::read_yaml::<Mapping>(dirs::clash_path())
-  }
-
-  /// save the clash config
-  pub fn save_config(&self) -> Result<()> {
-    config::save_yaml(
-      dirs::clash_path(),
-      &self.config,
-      Some("# Default Config For Clash Core\n\n"),
-    )
-  }
-
-  /// patch update the clash config
-  /// if the port is changed then return true
-  pub fn patch_config(&mut self, patch: Mapping) -> Result<bool> {
-    let port_key = Value::from("mixed-port");
-    let server_key = Value::from("external-controller");
-    let secret_key = Value::from("secret");
-
-    let mut change_port = false;
-    let mut change_info = false;
-
-    for (key, value) in patch.into_iter() {
-      if key == port_key {
-        change_port = true;
-      }
-
-      if key == port_key || key == server_key || key == secret_key {
-        change_info = true;
-      }
-
-      self.config.insert(key, value);
+        Clash { config, info }
     }
 
-    if change_info {
-      self.info = ClashInfo::from(&self.config);
+    /// get clash config
+    pub fn read_config() -> Mapping {
+        config::read_yaml::<Mapping>(dirs::clash_path())
     }
 
-    self.save_config()?;
-
-    Ok(change_port)
-  }
-
-  /// revise the `tun` and `dns` config
-  pub fn _tun_mode(mut config: Mapping, enable: bool) -> Mapping {
-    macro_rules! revise {
-      ($map: expr, $key: expr, $val: expr) => {
-        let ret_key = Value::String($key.into());
-        $map.insert(ret_key, Value::from($val));
-      };
+    /// save the clash config
+    pub fn save_config(&self) -> Result<()> {
+        config::save_yaml(
+            dirs::clash_path(),
+            &self.config,
+            Some("# Default Config For Clash Core\n\n"),
+        )
     }
 
-    // if key not exists then append value
-    macro_rules! append {
-      ($map: expr, $key: expr, $val: expr) => {
-        let ret_key = Value::String($key.into());
-        if !$map.contains_key(&ret_key) {
-          $map.insert(ret_key, Value::from($val));
+    /// patch update the clash config
+    /// if the port is changed then return true
+    pub fn patch_config(&mut self, patch: Mapping) -> Result<(bool, bool)> {
+        let port_key = Value::from("mixed-port");
+        let server_key = Value::from("external-controller");
+        let secret_key = Value::from("secret");
+        let mode_key = Value::from("mode");
+
+        let mut change_port = false;
+        let mut change_info = false;
+        let mut change_mode = false;
+
+        for (key, value) in patch.into_iter() {
+            if key == port_key {
+                change_port = true;
+            }
+
+            if key == mode_key {
+                change_mode = true;
+            }
+
+            if key == port_key || key == server_key || key == secret_key || key == mode_key {
+                change_info = true;
+            }
+
+            self.config.insert(key, value);
         }
-      };
-    }
 
-    // tun config
-    let tun_val = config.get(&Value::from("tun"));
-    let mut new_tun = Mapping::new();
-
-    if tun_val.is_some() && tun_val.as_ref().unwrap().is_mapping() {
-      new_tun = tun_val.as_ref().unwrap().as_mapping().unwrap().clone();
-    }
-
-    revise!(new_tun, "enable", enable);
-
-    if enable {
-      append!(new_tun, "stack", "gvisor");
-      append!(new_tun, "dns-hijack", vec!["198.18.0.2:53"]);
-      append!(new_tun, "auto-route", true);
-      append!(new_tun, "auto-detect-interface", true);
-    }
-
-    revise!(config, "tun", new_tun);
-
-    if enable {
-      // dns config
-      let dns_val = config.get(&Value::from("dns"));
-      let mut new_dns = Mapping::new();
-
-      if dns_val.is_some() && dns_val.as_ref().unwrap().is_mapping() {
-        new_dns = dns_val.as_ref().unwrap().as_mapping().unwrap().clone();
-      }
-      revise!(new_dns, "enable", enable);
-
-      // 借鉴cfw的默认配置
-      append!(new_dns, "enhanced-mode", "fake-ip");
-      append!(
-        new_dns,
-        "nameserver",
-        vec!["114.114.114.114", "223.5.5.5", "8.8.8.8"]
-      );
-      append!(new_dns, "fallback", vec![] as Vec<&str>);
-
-      #[cfg(target_os = "windows")]
-      append!(
-        new_dns,
-        "fake-ip-filter",
-        vec![
-          "dns.msftncsi.com",
-          "www.msftncsi.com",
-          "www.msftconnecttest.com"
-        ]
-      );
-
-      revise!(config, "dns", new_dns);
-    }
-
-    config
-  }
-
-  /// only 5 default fields available (clash config fields)
-  /// convert to lowercase
-  pub fn strict_filter(config: Mapping) -> Mapping {
-    // Only the following fields are allowed:
-    // proxies/proxy-providers/proxy-groups/rule-providers/rules
-    let valid_keys = vec![
-      "proxies",
-      "proxy-providers",
-      "proxy-groups",
-      "rules",
-      "rule-providers",
-    ];
-
-    let mut new_config = Mapping::new();
-
-    for (key, value) in config.into_iter() {
-      key.as_str().map(|key_str| {
-        // change to lowercase
-        let mut key_str = String::from(key_str);
-        key_str.make_ascii_lowercase();
-
-        // filter
-        if valid_keys.contains(&&*key_str) {
-          new_config.insert(Value::String(key_str), value);
+        if change_info {
+            self.info = ClashInfo::from(&self.config);
         }
-      });
+
+        self.save_config()?;
+
+        Ok((change_port, change_mode))
     }
 
-    new_config
-  }
-
-  /// more clash config fields available
-  /// convert to lowercase
-  pub fn loose_filter(config: Mapping) -> Mapping {
-    // all of these can not be revised by script or merge
-    // http/https/socks port should be under control
-    let not_allow = vec![
-      "port",
-      "socks-port",
-      "mixed-port",
-      "allow-lan",
-      "mode",
-      "external-controller",
-      "secret",
-      "log-level",
-    ];
-
-    let mut new_config = Mapping::new();
-
-    for (key, value) in config.into_iter() {
-      key.as_str().map(|key_str| {
-        // change to lowercase
-        let mut key_str = String::from(key_str);
-        key_str.make_ascii_lowercase();
-
-        // filter
-        if !not_allow.contains(&&*key_str) {
-          new_config.insert(Value::String(key_str), value);
+    /// revise the `tun` and `dns` config
+    pub fn _tun_mode(mut config: Mapping, enable: bool) -> Mapping {
+        macro_rules! revise {
+            ($map: expr, $key: expr, $val: expr) => {
+                let ret_key = Value::String($key.into());
+                $map.insert(ret_key, Value::from($val));
+            };
         }
-      });
+
+        // if key not exists then append value
+        macro_rules! append {
+            ($map: expr, $key: expr, $val: expr) => {
+                let ret_key = Value::String($key.into());
+                if !$map.contains_key(&ret_key) {
+                    $map.insert(ret_key, Value::from($val));
+                }
+            };
+        }
+
+        // tun config
+        let tun_val = config.get(&Value::from("tun"));
+        let mut new_tun = Mapping::new();
+
+        if tun_val.is_some() && tun_val.as_ref().unwrap().is_mapping() {
+            new_tun = tun_val.as_ref().unwrap().as_mapping().unwrap().clone();
+        }
+
+        revise!(new_tun, "enable", enable);
+
+        if enable {
+            append!(new_tun, "stack", "gvisor");
+            append!(new_tun, "dns-hijack", vec!["198.18.0.2:53"]);
+            append!(new_tun, "auto-route", true);
+            append!(new_tun, "auto-detect-interface", true);
+        }
+
+        revise!(config, "tun", new_tun);
+
+        if enable {
+            // dns config
+            let dns_val = config.get(&Value::from("dns"));
+            let mut new_dns = Mapping::new();
+
+            if dns_val.is_some() && dns_val.as_ref().unwrap().is_mapping() {
+                new_dns = dns_val.as_ref().unwrap().as_mapping().unwrap().clone();
+            }
+            revise!(new_dns, "enable", enable);
+
+            // 借鉴cfw的默认配置
+            append!(new_dns, "enhanced-mode", "fake-ip");
+            append!(
+                new_dns,
+                "nameserver",
+                vec!["114.114.114.114", "223.5.5.5", "8.8.8.8"]
+            );
+            append!(new_dns, "fallback", vec![] as Vec<&str>);
+
+            #[cfg(target_os = "windows")]
+            append!(
+                new_dns,
+                "fake-ip-filter",
+                vec![
+                    "dns.msftncsi.com",
+                    "www.msftncsi.com",
+                    "www.msftconnecttest.com"
+                ]
+            );
+
+            revise!(config, "dns", new_dns);
+        }
+
+        config
     }
 
-    new_config
-  }
+    /// only 5 default fields available (clash config fields)
+    /// convert to lowercase
+    pub fn strict_filter(config: Mapping) -> Mapping {
+        // Only the following fields are allowed:
+        // proxies/proxy-providers/proxy-groups/rule-providers/rules
+        let valid_keys = vec![
+            "proxies",
+            "proxy-providers",
+            "proxy-groups",
+            "rules",
+            "rule-providers",
+        ];
+
+        let mut new_config = Mapping::new();
+
+        for (key, value) in config.into_iter() {
+            key.as_str().map(|key_str| {
+                // change to lowercase
+                let mut key_str = String::from(key_str);
+                key_str.make_ascii_lowercase();
+
+                // filter
+                if valid_keys.contains(&&*key_str) {
+                    new_config.insert(Value::String(key_str), value);
+                }
+            });
+        }
+
+        new_config
+    }
+
+    /// more clash config fields available
+    /// convert to lowercase
+    pub fn loose_filter(config: Mapping) -> Mapping {
+        // all of these can not be revised by script or merge
+        // http/https/socks port should be under control
+        let not_allow = vec![
+            "port",
+            "socks-port",
+            "mixed-port",
+            "allow-lan",
+            "mode",
+            "external-controller",
+            "secret",
+            "log-level",
+        ];
+
+        let mut new_config = Mapping::new();
+
+        for (key, value) in config.into_iter() {
+            key.as_str().map(|key_str| {
+                // change to lowercase
+                let mut key_str = String::from(key_str);
+                key_str.make_ascii_lowercase();
+
+                // filter
+                if !not_allow.contains(&&*key_str) {
+                    new_config.insert(Value::String(key_str), value);
+                }
+            });
+        }
+
+        new_config
+    }
 }
 
 impl Default for Clash {
-  fn default() -> Self {
-    Clash::new()
-  }
+    fn default() -> Self {
+        Clash::new()
+    }
 }
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,6 @@
 #![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
 )]
 
 mod cmds;
@@ -8,156 +8,177 @@ mod core;
 mod utils;
 
 use crate::{
-  core::Verge,
-  utils::{resolve, server},
+    core::Verge,
+    utils::{resolve, server},
 };
+use serde_yaml::{Mapping, Value};
 use tauri::{
-  api, CustomMenuItem, Manager, SystemTray, SystemTrayEvent, SystemTrayMenu, SystemTrayMenuItem,
+    api, CustomMenuItem, Manager, SystemTray, SystemTrayEvent, SystemTrayMenu, SystemTrayMenuItem,
 };
 
 fn main() -> std::io::Result<()> {
-  if server::check_singleton().is_err() {
-    println!("app exists");
-    return Ok(());
-  }
+    if server::check_singleton().is_err() {
+        println!("app exists");
+        return Ok(());
+    }
 
-  #[cfg(target_os = "windows")]
-  unsafe {
-    use crate::utils::dirs;
+    #[cfg(target_os = "windows")]
+    unsafe {
+        use crate::utils::dirs;
 
-    dirs::init_portable_flag();
-  }
+        dirs::init_portable_flag();
+    }
 
-  let tray_menu = SystemTrayMenu::new()
-    .add_item(CustomMenuItem::new("open_window", "Show"))
-    .add_item(CustomMenuItem::new("system_proxy", "System Proxy"))
-    .add_item(CustomMenuItem::new("tun_mode", "Tun Mode"))
-    .add_item(CustomMenuItem::new("restart_clash", "Restart Clash"))
-    .add_native_item(SystemTrayMenuItem::Separator)
-    .add_item(CustomMenuItem::new("quit", "Quit").accelerator("CmdOrControl+Q"));
+    let tray_menu = SystemTrayMenu::new()
+        .add_item(CustomMenuItem::new("open_window", "Show"))
+        .add_item(CustomMenuItem::new("rule_mode", "Rule Mode"))
+        .add_item(CustomMenuItem::new("global_mode", "Global Mode"))
+        .add_item(CustomMenuItem::new("direct_mode", "Direct Mode"))
+        .add_item(CustomMenuItem::new("script_mode", "Script Mode"))
+        .add_item(CustomMenuItem::new("system_proxy", "System Proxy"))
+        .add_item(CustomMenuItem::new("tun_mode", "Tun Mode"))
+        .add_item(CustomMenuItem::new("restart_clash", "Restart Clash"))
+        .add_native_item(SystemTrayMenuItem::Separator)
+        .add_item(CustomMenuItem::new("quit", "Quit").accelerator("CmdOrControl+Q"));
 
-  #[allow(unused_mut)]
-  let mut builder = tauri::Builder::default()
-    .setup(|app| Ok(resolve::resolve_setup(app)))
-    .system_tray(SystemTray::new().with_menu(tray_menu))
-    .on_system_tray_event(move |app_handle, event| match event {
-      SystemTrayEvent::MenuItemClick { id, .. } => match id.as_str() {
-        "open_window" => {
-          resolve::create_window(app_handle);
-        }
-        "system_proxy" => {
-          let core = app_handle.state::<core::Core>();
+    #[allow(unused_mut)]
+    let mut builder = tauri::Builder::default()
+        .setup(|app| Ok(resolve::resolve_setup(app)))
+        .system_tray(SystemTray::new().with_menu(tray_menu))
+        .on_system_tray_event(move |app_handle, event| match event {
+            SystemTrayEvent::MenuItemClick { id, .. } => match id.as_str() {
+                "open_window" => {
+                    resolve::create_window(app_handle);
+                }
+                "rule_mode" => {
+                    let core = app_handle.state::<core::Core>();
+                    crate::log_if_err!(core.update_mode(app_handle, "rule"));
+                }
+                "global_mode" => {
+                    let core = app_handle.state::<core::Core>();
+                    crate::log_if_err!(core.update_mode(app_handle, "global"));
+                }
+                "direct_mode" => {
+                    let core = app_handle.state::<core::Core>();
+                    crate::log_if_err!(core.update_mode(app_handle, "direct"));
+                }
+                "script_mode" => {
+                    let core = app_handle.state::<core::Core>();
+                    crate::log_if_err!(core.update_mode(app_handle, "script"));
+                }
+                "system_proxy" => {
+                    let core = app_handle.state::<core::Core>();
 
-          let new_value = {
-            let verge = core.verge.lock();
-            !verge.enable_system_proxy.clone().unwrap_or(false)
-          };
+                    let new_value = {
+                        let verge = core.verge.lock();
+                        !verge.enable_system_proxy.clone().unwrap_or(false)
+                    };
 
-          let patch = Verge {
-            enable_system_proxy: Some(new_value),
-            ..Verge::default()
-          };
+                    let patch = Verge {
+                        enable_system_proxy: Some(new_value),
+                        ..Verge::default()
+                    };
 
-          crate::log_if_err!(core.patch_verge(patch, app_handle));
-        }
-        "tun_mode" => {
-          let core = app_handle.state::<core::Core>();
+                    crate::log_if_err!(core.patch_verge(patch, app_handle));
+                }
+                "tun_mode" => {
+                    let core = app_handle.state::<core::Core>();
 
-          let new_value = {
-            let verge = core.verge.lock();
-            !verge.enable_tun_mode.clone().unwrap_or(false)
-          };
+                    let new_value = {
+                        let verge = core.verge.lock();
+                        !verge.enable_tun_mode.clone().unwrap_or(false)
+                    };
 
-          let patch = Verge {
-            enable_tun_mode: Some(new_value),
-            ..Verge::default()
-          };
+                    let patch = Verge {
+                        enable_tun_mode: Some(new_value),
+                        ..Verge::default()
+                    };
 
-          crate::log_if_err!(core.patch_verge(patch, app_handle));
-        }
-        "restart_clash" => {
-          let core = app_handle.state::<core::Core>();
-          crate::log_if_err!(core.restart_clash());
-        }
-        "quit" => {
-          resolve::resolve_reset(app_handle);
-          app_handle.exit(0);
-        }
-        _ => {}
-      },
-      #[cfg(target_os = "windows")]
-      SystemTrayEvent::LeftClick { .. } => {
-        resolve::create_window(app_handle);
-      }
-      _ => {}
-    })
-    .invoke_handler(tauri::generate_handler![
-      // common
-      cmds::get_sys_proxy,
-      cmds::get_cur_proxy,
-      cmds::open_app_dir,
-      cmds::open_logs_dir,
-      cmds::kill_sidecar,
-      cmds::restart_sidecar,
-      // clash
-      cmds::get_clash_info,
-      cmds::patch_clash_config,
-      cmds::change_clash_core,
-      // verge
-      cmds::get_verge_config,
-      cmds::patch_verge_config,
-      // profile
-      cmds::view_profile,
-      cmds::patch_profile,
-      cmds::create_profile,
-      cmds::import_profile,
-      cmds::update_profile,
-      cmds::delete_profile,
-      cmds::select_profile,
-      cmds::get_profiles,
-      cmds::enhance_profiles,
-      cmds::change_profile_chain,
-      cmds::change_profile_valid,
-      cmds::read_profile_file,
-      cmds::save_profile_file,
-      // service mode
-      cmds::service::start_service,
-      cmds::service::stop_service,
-      cmds::service::check_service,
-      cmds::service::install_service,
-      cmds::service::uninstall_service,
-    ]);
+                    crate::log_if_err!(core.patch_verge(patch, app_handle));
+                }
+                "restart_clash" => {
+                    let core = app_handle.state::<core::Core>();
+                    crate::log_if_err!(core.restart_clash());
+                }
+                "quit" => {
+                    resolve::resolve_reset(app_handle);
+                    app_handle.exit(0);
+                }
+                _ => {}
+            },
+            #[cfg(target_os = "windows")]
+            SystemTrayEvent::LeftClick { .. } => {
+                resolve::create_window(app_handle);
+            }
+            _ => {}
+        })
+        .invoke_handler(tauri::generate_handler![
+            // common
+            cmds::get_sys_proxy,
+            cmds::get_cur_proxy,
+            cmds::open_app_dir,
+            cmds::open_logs_dir,
+            cmds::kill_sidecar,
+            cmds::restart_sidecar,
+            // clash
+            cmds::get_clash_info,
+            cmds::patch_clash_config,
+            cmds::change_clash_core,
+            // verge
+            cmds::get_verge_config,
+            cmds::patch_verge_config,
+            // profile
+            cmds::view_profile,
+            cmds::patch_profile,
+            cmds::create_profile,
+            cmds::import_profile,
+            cmds::update_profile,
+            cmds::delete_profile,
+            cmds::select_profile,
+            cmds::get_profiles,
+            cmds::enhance_profiles,
+            cmds::change_profile_chain,
+            cmds::change_profile_valid,
+            cmds::read_profile_file,
+            cmds::save_profile_file,
+            // service mode
+            cmds::service::start_service,
+            cmds::service::stop_service,
+            cmds::service::check_service,
+            cmds::service::install_service,
+            cmds::service::uninstall_service,
+        ]);
 
-  #[cfg(target_os = "macos")]
-  {
-    use tauri::{Menu, MenuItem, Submenu};
+    #[cfg(target_os = "macos")]
+    {
+        use tauri::{Menu, MenuItem, Submenu};
 
-    let submenu_file = Submenu::new(
-      "File",
-      Menu::new()
-        .add_native_item(MenuItem::Undo)
-        .add_native_item(MenuItem::Redo)
-        .add_native_item(MenuItem::Copy)
-        .add_native_item(MenuItem::Paste)
-        .add_native_item(MenuItem::Cut)
-        .add_native_item(MenuItem::SelectAll),
-    );
-    builder = builder.menu(Menu::new().add_submenu(submenu_file));
-  }
+        let submenu_file = Submenu::new(
+            "File",
+            Menu::new()
+                .add_native_item(MenuItem::Undo)
+                .add_native_item(MenuItem::Redo)
+                .add_native_item(MenuItem::Copy)
+                .add_native_item(MenuItem::Paste)
+                .add_native_item(MenuItem::Cut)
+                .add_native_item(MenuItem::SelectAll),
+        );
+        builder = builder.menu(Menu::new().add_submenu(submenu_file));
+    }
 
-  builder
-    .build(tauri::generate_context!())
-    .expect("error while running tauri application")
-    .run(|app_handle, e| match e {
-      tauri::RunEvent::ExitRequested { api, .. } => {
-        api.prevent_exit();
-      }
-      tauri::RunEvent::Exit => {
-        resolve::resolve_reset(app_handle);
-        api::process::kill_children();
-      }
-      _ => {}
-    });
+    builder
+        .build(tauri::generate_context!())
+        .expect("error while running tauri application")
+        .run(|app_handle, e| match e {
+            tauri::RunEvent::ExitRequested { api, .. } => {
+                api.prevent_exit();
+            }
+            tauri::RunEvent::Exit => {
+                resolve::resolve_reset(app_handle);
+                api::process::kill_children();
+            }
+            _ => {}
+        });
 
-  Ok(())
+    Ok(())
 }
