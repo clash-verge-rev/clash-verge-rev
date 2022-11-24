f76890cc56f4a5c1e8ca1f37fdd83568a288540b
GyDi <zzzgydi@gmail.com>
2022-11-24 11:11:31 +0800

    fix: parse bytes base 1024

--- src/utils/parse-traffic.ts
+++ src/utils/parse-traffic.ts
@@ -2,8 +2,8 @@ const UNITS = ["B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
 
 const parseTraffic = (num: number) => {
   if (num < 1000) return [`${Math.round(num)}`, "B"];
-  const exp = Math.min(Math.floor(Math.log10(num) / 3), UNITS.length - 1);
-  const ret = (num / Math.pow(1000, exp)).toPrecision(3);
+  const exp = Math.min(Math.floor(Math.log2(num) / 10), UNITS.length - 1);
+  const ret = (num / Math.pow(1024, exp)).toPrecision(3);
   const unit = UNITS[exp];
 
   return [ret, unit];
