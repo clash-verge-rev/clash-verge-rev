829361d767112b013040b88d5f4c655d3d6385ce
GyDi <zzzgydi@gmail.com>
2023-01-11 14:05:49 +0800

    fix: parse bytes precision, close #334

--- src/utils/parse-traffic.ts
+++ src/utils/parse-traffic.ts
@@ -3,7 +3,8 @@ const UNITS = ["B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
 const parseTraffic = (num: number) => {
   if (num < 1000) return [`${Math.round(num)}`, "B"];
   const exp = Math.min(Math.floor(Math.log2(num) / 10), UNITS.length - 1);
-  const ret = (num / Math.pow(1024, exp)).toPrecision(3);
+  const dat = num / Math.pow(1024, exp);
+  const ret = dat >= 1000 ? dat.toFixed(0) : dat.toPrecision(3);
   const unit = UNITS[exp];
 
   return [ret, unit];
