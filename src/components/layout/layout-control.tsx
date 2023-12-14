2e25a4333acd845368825f4a0bbcd1a05011b939
MystiPanda <mystipanda@proton.me>
2023-12-14 14:59:55 +0800

    fix: Window control button icon issue
    #136

--- src/components/layout/layout-control.tsx
+++ src/components/layout/layout-control.tsx
@@ -3,11 +3,18 @@ import { appWindow } from "@tauri-apps/api/window";
 import {
   CloseRounded,
   CropSquareRounded,
+  FilterNoneRounded,
   HorizontalRuleRounded,
 } from "@mui/icons-material";
+import { useState } from "react";
 
 export const LayoutControl = () => {
   const minWidth = 40;
+
+  const [isMaximized, setIsMaximized] = useState(false);
+  appWindow.isMaximized().then((isMaximized) => {
+    setIsMaximized(() => isMaximized);
+  });
 
   return (
     <>
@@ -22,9 +29,21 @@ export const LayoutControl = () => {
       <Button
         size="small"
         sx={{ minWidth, svg: { transform: "scale(0.9)" } }}
-        onClick={() => appWindow.toggleMaximize()}
+        onClick={() => {
+          setIsMaximized((isMaximized) => !isMaximized);
+          appWindow.toggleMaximize();
+        }}
       >
-        <CropSquareRounded fontSize="small" />
+        {isMaximized ? (
+          <FilterNoneRounded
+            fontSize="small"
+            style={{
+              transform: "rotate(180deg) scale(0.7)",
+            }}
+          />
+        ) : (
+          <CropSquareRounded fontSize="small" />
+        )}
       </Button>
 
       <Button
