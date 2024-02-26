de1924cefcbaa55cd1a8ee46024b42cdad7e8ec1
MystiPanda <mystipanda@proton.me>
2024-02-26 13:31:51 +0800

    fix: Image display failed on Linux

--- scripts/check.mjs
+++ scripts/check.mjs
@@ -13,20 +13,24 @@ const FORCE = process.argv.includes("--force");
 
 const PLATFORM_MAP = {
   "x86_64-pc-windows-msvc": "win32",
+  "i686-pc-windows-msvc": "win32",
   "aarch64-pc-windows-msvc": "win32",
   "x86_64-apple-darwin": "darwin",
   "aarch64-apple-darwin": "darwin",
   "x86_64-unknown-linux-gnu": "linux",
+  "i686-unknown-linux-gnu": "linux",
   "aarch64-unknown-linux-gnu": "linux",
   "armv7-unknown-linux-gnueabihf": "linux",
   "loongarch64-unknown-linux-gnu": "linux",
 };
 const ARCH_MAP = {
   "x86_64-pc-windows-msvc": "x64",
+  "i686-pc-windows-msvc": "ia32",
   "aarch64-pc-windows-msvc": "arm64",
   "x86_64-apple-darwin": "x64",
   "aarch64-apple-darwin": "arm64",
   "x86_64-unknown-linux-gnu": "x64",
+  "i686-unknown-linux-gnu": "ia32",
   "aarch64-unknown-linux-gnu": "arm64",
   "armv7-unknown-linux-gnueabihf": "arm",
   "loongarch64-unknown-linux-gnu": "loong64",
@@ -53,10 +57,12 @@ let META_ALPHA_VERSION;
 
 const META_ALPHA_MAP = {
   "win32-x64": "mihomo-windows-amd64-compatible",
+  "win32-ia32": "mihomo-windows-386",
   "win32-arm64": "mihomo-windows-arm64",
   "darwin-x64": "mihomo-darwin-amd64",
   "darwin-arm64": "mihomo-darwin-arm64",
   "linux-x64": "mihomo-linux-amd64-compatible",
+  "linux-ia32": "mihomo-linux-386",
   "linux-arm64": "mihomo-linux-arm64",
   "linux-arm": "mihomo-linux-armv7",
   "linux-loong64": "mihomo-linux-loong64",
@@ -97,10 +103,12 @@ let META_VERSION;
 
 const META_MAP = {
   "win32-x64": "mihomo-windows-amd64-compatible",
+  "win32-ia32": "mihomo-windows-386",
   "win32-arm64": "mihomo-windows-arm64",
   "darwin-x64": "mihomo-darwin-amd64",
   "darwin-arm64": "mihomo-darwin-arm64",
   "linux-x64": "mihomo-linux-amd64-compatible",
+  "linux-ia32": "mihomo-linux-386",
   "linux-arm64": "mihomo-linux-arm64",
   "linux-arm": "mihomo-linux-armv7",
   "linux-loong64": "mihomo-linux-loong64",
