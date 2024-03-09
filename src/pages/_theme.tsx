e98acf5c325f8359a68ada7024fb296db390e173
Amnesiash <wangjh9701@qq.com>
2024-03-09 21:26:08 +0800

    update theme color

--- src/pages/_theme.tsx
+++ src/pages/_theme.tsx
@@ -3,14 +3,14 @@ const OS = getSystem();
 
 // default theme setting
 export const defaultTheme = {
-  primary_color: "#1890ff",
-  secondary_color: "#f5222d",
-  primary_text: "#1f1f1f",
-  secondary_text: "#8c8c8c",
-  info_color: "#1677ff",
-  error_color: "#f5222d",
-  warning_color: "#faad14",
-  success_color: "#52c41a",
+  primary_color: "#007AFF",
+  secondary_color: "#FFCC00",
+  primary_text: "#000000",
+  secondary_text: "rgba(60, 60, 67, 0.60)",
+  info_color: "#007AFF",
+  error_color: "#FF3B30",
+  warning_color: "#FF9500",
+  success_color: "#34C759",
   background_color: "#f5f5f5",
   font_family: `"Roboto", "Helvetica", "Arial", sans-serif, ${
     OS === "windows" ? "twemoji mozilla" : ""
@@ -20,7 +20,13 @@ export const defaultTheme = {
 // dark mode
 export const defaultDarkTheme = {
   ...defaultTheme,
+  primary_color: "#0A84FF",
+  secondary_color: "#FF9F0A",
   primary_text: "#ffffff",
   background_color: "#2e303d",
-  secondary_text: "#ffffff",
+  secondary_text: "rgba(235, 235, 245, 0.60)",
+  info_color: "#0A84FF",
+  error_color: "#FF453A",
+  warning_color: "#FF9F0A",
+  success_color: "#30D158",
 };
