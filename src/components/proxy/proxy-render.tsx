52631832ddcc6672dfd4db923519941f929be33b
MystiPanda <mystipanda@proton.me>
2024-03-09 19:47:43 +0800

    style: change dark mode color for proxy group

--- src/components/proxy/proxy-render.tsx
+++ src/components/proxy/proxy-render.tsx
@@ -38,7 +38,7 @@ export const ProxyRender = (props: RenderProps) => {
   const [mode] = useRecoilState(atomThemeMode);
   console.log(mode);
   const isDark = mode === "light" ? false : true;
-  const itembackgroundcolor = isDark ? "#191a21" : "#ffffff";
+  const itembackgroundcolor = isDark ? "#2E303D" : "#ffffff";
 
   if (type === 0 && !group.hidden) {
     return (
