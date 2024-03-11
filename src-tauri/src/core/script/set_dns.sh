97bee17e4e427078938a8c3d87d537bb709871ab
MystiPanda <mystipanda@proton.me>
2024-03-11 16:05:09 +0800

    chore: Change IP

--- src-tauri/src/core/script/set_dns.sh
+++ src-tauri/src/core/script/set_dns.sh
@@ -2,4 +2,4 @@ nic=$(route -n get default | grep "interface" | awk '{print $2}')
 
 hardware_port=$(networksetup -listallhardwareports | awk -v dev="$nic" '/Hardware Port/{port=$3} /Device:/{if ($2 == dev) {print port; exit}}')
 
-networksetup -setdnsservers $hardware_port 192.18.0.2
+networksetup -setdnsservers $hardware_port 223.5.5.5
