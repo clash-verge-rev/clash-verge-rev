39a9181cddbfb496b8e45d8d180b392caaaa0f88
MystiPanda <mystipanda@proton.me>
2024-05-08 00:22:14 +0800

    refactor: remove prepend and append for provider

--- src-tauri/src/enhance/merge.rs
+++ src-tauri/src/enhance/merge.rs
@@ -1,15 +1,11 @@
 use super::{use_filter, use_lowercase};
 use serde_yaml::{self, Mapping, Sequence, Value};
 
-const MERGE_FIELDS: [&str; 10] = [
+const MERGE_FIELDS: [&str; 6] = [
     "prepend-rules",
     "append-rules",
-    "prepend-rule-providers",
-    "append-rule-providers",
     "prepend-proxies",
     "append-proxies",
-    "prepend-proxy-providers",
-    "append-proxy-providers",
     "prepend-proxy-groups",
     "append-proxy-groups",
 ];
@@ -36,39 +32,6 @@ pub fn use_merge(merge: Mapping, config: Mapping) -> Mapping {
     let mut config = config.as_mapping().unwrap().clone();
     let merge_list = MERGE_FIELDS.iter().map(|s| s.to_string());
     let merge = use_filter(merge, &merge_list.collect());
-
-    ["rule-providers", "proxy-providers"]
-        .iter()
-        .for_each(|key_str| {
-            let key_val = Value::from(key_str.to_string());
-
-            let mut map = Mapping::default();
-
-            map = config.get(&key_val).map_or(map.clone(), |val| {
-                val.as_mapping().map_or(map, |v| v.clone())
-            });
-
-            let pre_key = Value::from(format!("prepend-{key_str}"));
-            let post_key = Value::from(format!("append-{key_str}"));
-
-            if let Some(pre_val) = merge.get(&pre_key) {
-                if pre_val.is_mapping() {
-                    let mut pre_val = pre_val.as_mapping().unwrap().clone();
-                    pre_val.extend(map);
-                    map = pre_val;
-                }
-            }
-
-            if let Some(post_val) = merge.get(&post_key) {
-                if post_val.is_mapping() {
-                    map.extend(post_val.as_mapping().unwrap().clone());
-                }
-            }
-
-            if !map.is_empty() {
-                config.insert(key_val, Value::from(map));
-            }
-        });
 
     ["rules", "proxies", "proxy-groups"]
         .iter()
