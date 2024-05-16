284438386023301520f15e2d0c70919442250e26
oomeow <oomeow@outlook.com>
2024-05-16 18:26:50 +0800

    chore: optimize arch build script

--- archbuild/local_build/build.sh
+++ archbuild/local_build/build.sh
@@ -1,3 +1,8 @@
+read -p "rebuild deb package? (y/n): " rebuild
+if [[ "$rebuild" =~ ^[Yy]$ ]]; then
+    pnpm build -b deb
+fi
+
 CURRENT_SCRIPT_DIR=$(
     cd "$(dirname "$0")" || exit 1
     pwd
@@ -9,10 +14,17 @@ PROJECT_PACKAGEJSON_PATH="$PROJECT_ROOT_DIR/package.json"
 VERSION=$(cat ${PROJECT_PACKAGEJSON_PATH} | jq '.version' | tr -d '"')
 PROJECT_RELEASE_DEB_PATH="$PROJECT_ROOT_DIR/src-tauri/target/release/bundle/deb/clash-verge_${VERSION}_amd64.deb"
 
-if [ -e "${PROJECT_RELEASE_DEB_PATH}" ]; then
-    cp ${PROJECT_ROOT_DIR}/src-tauri/target/release/bundle/deb/clash-verge_${VERSION}_amd64.deb .
-else
-    pnpm build -b deb
-fi
+cp ${PROJECT_RELEASE_DEB_PATH} .
 
 makepkg -fc
+
+read -p "install now? (y/n): " yay_install
+
+ARCH_PKG_VERSION=$(grep "^pkgver=" ${CURRENT_SCRIPT_DIR}/PKGBUILD | sed 's/^pkgver=//')
+if [[ "$yay_install" =~ ^[Yy]$ ]]; then
+    echo "installing..."
+    yay -U clash-verge-rev-patch-alpha-bin-${ARCH_PKG_VERSION}-1-x86_64.pkg.tar.zst --noconfirm
+    echo "install finished."
+else
+    echo "skip install."
+fi
