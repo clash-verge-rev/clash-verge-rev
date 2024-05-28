7be31c04426b66e6856e1bb408ea87063020bd0d
shengchao <749312025@qq.com>
2024-05-28 18:12:59 +0800

    feat: Use the official vite plugin @vitejs/plugin-legacy instead of babel.

--- vite.config.ts
+++ vite.config.ts
@@ -2,26 +2,17 @@ import { defineConfig } from "vite";
 import path from "path";
 import svgr from "vite-plugin-svgr";
 import react from "@vitejs/plugin-react";
-import { babel } from "@rollup/plugin-babel";
 import monacoEditor from "vite-plugin-monaco-editor";
-
+import legacy from "@vitejs/plugin-legacy";
 export default defineConfig({
   root: "src",
   server: { port: 3000 },
   plugins: [
     svgr(),
     react(),
-    {
-      apply: "build", // apply only for build, not for serve
-      ...babel({
-        babelHelpers: "bundled",
-        extensions: [".js", ".jsx", ".ts", ".tsx"],
-        targets: {
-          edge: "109", // last version to support Windows 7
-          safari: "13", // macOS 10.15 Catalina
-        },
-      }),
-    },
+    legacy({
+      targets: ["edge >= 109", "safari >= 13"],
+    }),
     monacoEditor({
       languageWorkers: ["editorWorkerService", "typescript", "css"],
       customWorkers: [
