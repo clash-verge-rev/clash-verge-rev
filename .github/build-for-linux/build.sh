2a5b70fb136edf6dcad8492fd8cf4f10c66dd687
MystiPanda <mystipanda@proton.me>
2024-06-04 10:16:25 +0800

    feat: support rpm package

--- .github/build-for-linux/build.sh
+++ .github/build-for-linux/build.sh
@@ -4,5 +4,5 @@ sed -i "s/#openssl/openssl={version=\"0.10\",features=[\"vendored\"]}/g" src-tau
 if [ "$INPUT_TARGET" = "x86_64-unknown-linux-gnu" ]; then
     cargo tauri build --target $INPUT_TARGET
 else
-    cargo tauri build --target $INPUT_TARGET -b deb
+    cargo tauri build --target $INPUT_TARGET -b deb,rpm
 fi
