9fe4197caea35c788a65b684962e676df81c884b
dongchengjie <37543964+dongchengjie@users.noreply.github.com>
2024-06-05 14:20:24 +0800

    chore: theme colors to uppercase

--- src/pages/_theme.tsx
+++ src/pages/_theme.tsx
@@ -4,14 +4,14 @@ const OS = getSystem();
 // default theme setting
 export const defaultTheme = {
   primary_color: "#007AFF",
-  secondary_color: "#fc9b76",
+  secondary_color: "#FC9B76",
   primary_text: "#000000",
-  secondary_text: "#3c3c4399",
+  secondary_text: "#3C3C4399",
   info_color: "#007AFF",
   error_color: "#FF3B30",
   warning_color: "#FF9500",
   success_color: "#06943D",
-  background_color: "#f5f5f5",
+  background_color: "#F5F5F5",
   font_family: `-apple-system, BlinkMacSystemFont,"Microsoft YaHei UI", "Microsoft YaHei", Roboto, "Helvetica Neue", Arial, sans-serif, "Apple Color Emoji"${
     OS === "windows" ? ", twemoji mozilla" : ""
   }`,
@@ -22,9 +22,9 @@ export const defaultDarkTheme = {
   ...defaultTheme,
   primary_color: "#0A84FF",
   secondary_color: "#FF9F0A",
-  primary_text: "#ffffff",
-  background_color: "#2e303d",
-  secondary_text: "#ebebf599",
+  primary_text: "#FFFFFF",
+  background_color: "#2E303D",
+  secondary_text: "#EBEBF599",
   info_color: "#0A84FF",
   error_color: "#FF453A",
   warning_color: "#FF9F0A",
