c0219662bbe5cdda4a7874a43c7167fe3cd50ac3
MystiPanda <mystipanda@proton.me>
2024-06-08 19:36:58 +0800

    build: remove appimage

--- .github/build-for-linux/build.sh
+++ .github/build-for-linux/build.sh
@@ -1,8 +1,5 @@
 pnpm install
 pnpm check $INPUT_TARGET
 sed -i "s/#openssl/openssl={version=\"0.10\",features=[\"vendored\"]}/g" src-tauri/Cargo.toml
-if [ "$INPUT_TARGET" = "x86_64-unknown-linux-gnu" ]; then
-    cargo tauri build --target $INPUT_TARGET
-else
-    cargo tauri build --target $INPUT_TARGET -b deb,rpm
-fi
+
+cargo tauri build --target $INPUT_TARGET
