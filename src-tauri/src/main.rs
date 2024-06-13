b5556613cff40548762581486632db5b4c9cf152
MystiPanda <mystipanda@proton.me>
2024-06-13 16:07:56 +0800

    refactor: remove grant logic

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -44,7 +44,6 @@ fn main() -> std::io::Result<()> {
             cmds::get_portable_flag,
             // cmds::kill_sidecar,
             cmds::restart_sidecar,
-            cmds::grant_permission,
             // clash
             cmds::get_clash_info,
             cmds::get_clash_logs,
