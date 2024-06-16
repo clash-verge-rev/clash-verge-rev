a951bf5a6439c87fd6b9a49dce4f615476ba560e
oomeow <oomeow@outlook.com>
2024-06-16 22:50:14 +0800

    chore: adjust profile style

--- src/components/profile/profile-box.tsx
+++ src/components/profile/profile-box.tsx
@@ -1,57 +1,65 @@
 import { alpha, Box, styled } from "@mui/material";
 
-export const ProfileBox = styled(Box)(
-  ({ theme, "aria-selected": selected }) => {
-    const { mode, primary, text } = theme.palette;
-    const key = `${mode}-${!!selected}`;
+export const ProfileBox = styled(Box)(({
+  theme,
+  "aria-selected": selected,
+}) => {
+  const { mode, primary, text } = theme.palette;
+  const key = `${mode}-${!!selected}`;
 
-    const backgroundColor = mode === "light" ? "#ffffff" : "#282A36";
+  const unselectedbackgroundColor = mode === "light" ? "#ffffff" : "#282A36";
+  const selectedBackgroundColor =
+    mode === "light" ? alpha(primary.main, 0.25) : alpha(primary.main, 0.35);
+  const backgroundColor = selected
+    ? selectedBackgroundColor
+    : unselectedbackgroundColor;
 
-    const color = {
-      "light-true": text.secondary,
-      "light-false": text.secondary,
-      "dark-true": alpha(text.secondary, 0.65),
-      "dark-false": alpha(text.secondary, 0.65),
-    }[key]!;
+  const color = {
+    "light-true": text.secondary,
+    "light-false": text.secondary,
+    "dark-true": alpha(text.secondary, 0.65),
+    "dark-false": alpha(text.secondary, 0.65),
+  }[key]!;
 
-    const h2color = {
-      "light-true": primary.main,
-      "light-false": text.primary,
-      "dark-true": primary.main,
-      "dark-false": text.primary,
-    }[key]!;
+  const h2color = {
+    "light-true": primary.main,
+    "light-false": text.primary,
+    "dark-true": primary.main,
+    "dark-false": text.primary,
+  }[key]!;
 
-    const borderSelect = {
-      "light-true": {
-        borderLeft: `3px solid ${primary.main}`,
-        width: `calc(100% + 3px)`,
-        marginLeft: `-3px`,
-      },
-      "light-false": {
-        width: "100%",
-      },
-      "dark-true": {
-        borderLeft: `3px solid ${primary.main}`,
-        width: `calc(100% + 3px)`,
-        marginLeft: `-3px`,
-      },
-      "dark-false": {
-        width: "100%",
-      },
-    }[key];
+  const borderSelect = {
+    "light-true": {
+      borderLeft: `3px solid ${primary.main}`,
+      width: "100%",
+      // marginLeft: `-3px`,
+    },
+    "light-false": {
+      width: "100%",
+    },
+    "dark-true": {
+      borderLeft: `3px solid ${primary.main}`,
+      width: "100%",
+      // marginLeft: `-3px`,
+    },
+    "dark-false": {
+      width: "100%",
+    },
+  }[key];
 
-    return {
-      position: "relative",
-      display: "block",
-      cursor: "pointer",
-      textAlign: "left",
-      padding: "8px 16px",
-      boxSizing: "border-box",
-      backgroundColor,
-      ...borderSelect,
-      borderRadius: "8px",
-      color,
-      "& h2": { color: h2color },
-    };
-  }
-);
+  return {
+    position: "relative",
+    display: "block",
+    cursor: "pointer",
+    textAlign: "left",
+    padding: "8px 16px",
+    boxSizing: "border-box",
+    backgroundColor,
+    ...borderSelect,
+    borderRadius: "8px",
+    color,
+    overflow: "hidden",
+    "& h2": { color: h2color },
+    transition: "all 0.3s ease",
+  };
+});
