514b392441abf8f78305591a2d326324591a37e9
SukkaW <isukkaw@gmail.com>
2024-06-16 18:09:22 +0800

    perf: reduce retry

--- src/utils/websocket.ts
+++ src/utils/websocket.ts
@@ -12,6 +12,9 @@ export const createSockette = (
 
   return new Sockette(url, {
     ...opt,
+    // Sockette has a built-in reconnect when ECONNREFUSED feature
+    // Use maxError if opt.maxAttempts is not specified
+    maxAttempts: opt.maxAttempts ?? maxError,
     onmessage(this: Sockette, ev) {
       remainRetryCount = maxError; // reset counter
       opt.onmessage?.call(this, ev);
@@ -25,6 +28,12 @@ export const createSockette = (
       } else {
         opt.onerror?.call(this, ev);
       }
+    },
+    onmaximum(this: Sockette, ev) {
+      opt.onmaximum?.call(this, ev);
+      // onmaximum will be fired when Sockette reaches built-in reconnect limit,
+      // We will also set remainRetryCount to 0 to prevent further reconnect.
+      remainRetryCount = 0;
     },
   });
 };
