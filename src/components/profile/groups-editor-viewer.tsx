47bacdaed0fc808cab84a5370a2aa550a5d6912d
MystiPanda <mystipanda@proton.me>
2024-07-04 14:18:21 +0800

    fix: locale typo

--- src/components/profile/groups-editor-viewer.tsx
+++ src/components/profile/groups-editor-viewer.tsx
@@ -58,7 +58,7 @@ export const GroupsEditorViewer = (props: Props) => {
   const themeMode = useThemeMode();
   const [prevData, setPrevData] = useState("");
   const [currData, setCurrData] = useState("");
-  const [visible, setVisible] = useState(true);
+  const [visualization, setVisualization] = useState(true);
   const [match, setMatch] = useState(() => (_: string) => true);
 
   const { control, watch, register, ...formIns } = useForm<IProxyGroupConfig>({
@@ -147,7 +147,7 @@ export const GroupsEditorViewer = (props: Props) => {
 
   useEffect(() => {
     if (currData === "") return;
-    if (visible !== true) return;
+    if (visualization !== true) return;
 
     let obj = yaml.load(currData) as {
       prepend: [];
@@ -157,7 +157,7 @@ export const GroupsEditorViewer = (props: Props) => {
     setPrependSeq(obj?.prepend || []);
     setAppendSeq(obj?.append || []);
     setDeleteSeq(obj?.delete || []);
-  }, [visible]);
+  }, [visualization]);
 
   useEffect(() => {
     if (prependSeq && appendSeq && deleteSeq)
@@ -273,10 +273,10 @@ export const GroupsEditorViewer = (props: Props) => {
                 variant="contained"
                 size="small"
                 onClick={() => {
-                  setVisible((prev) => !prev);
+                  setVisualization((prev) => !prev);
                 }}
               >
-                {visible ? t("Advanced") : t("Visible")}
+                {visualization ? t("Advanced") : t("Visualization")}
               </Button>
             </Box>
           </Box>
@@ -286,7 +286,7 @@ export const GroupsEditorViewer = (props: Props) => {
       <DialogContent
         sx={{ display: "flex", width: "auto", height: "calc(100vh - 185px)" }}
       >
-        {visible ? (
+        {visualization ? (
           <>
             <List
               sx={{
