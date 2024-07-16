7e0e302d485b296efab3bece65bf46aa479cbb3e
MystiPanda <mystipanda@proton.me>
2024-07-16 11:28:04 +0800

    fix: try to fix install service

---
+++ src/components/setting/mods/password-input.tsx
@@ -0,0 +1,52 @@
+import { useEffect, useState } from "react";
+import { useTranslation } from "react-i18next";
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  TextField,
+} from "@mui/material";
+
+interface Props {
+  onConfirm: (passwd: string) => Promise<void>;
+}
+
+export const PasswordInput = (props: Props) => {
+  const { onConfirm } = props;
+
+  const { t } = useTranslation();
+  const [passwd, setPasswd] = useState("");
+
+  useEffect(() => {
+    if (!open) return;
+  }, [open]);
+
+  return (
+    <Dialog open={true} maxWidth="xs" fullWidth>
+      <DialogTitle>{t("Please enter your root password")}</DialogTitle>
+
+      <DialogContent>
+        <TextField
+          sx={{ mt: 1 }}
+          label={t("Password")}
+          fullWidth
+          size="small"
+          type="password"
+          value={passwd}
+          onChange={(e) => setPasswd(e.target.value)}
+        ></TextField>
+      </DialogContent>
+
+      <DialogActions>
+        <Button
+          onClick={async () => await onConfirm(passwd)}
+          variant="contained"
+        >
+          {t("Confirm")}
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
