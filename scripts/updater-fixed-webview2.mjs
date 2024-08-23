d45929c604ef66985429c21fd350bc2a7f3d87d5
wonfen <eiwonfen@gmail.com>
2024-08-24 02:02:17 +0800

    chore: use in-house update proxy

--- scripts/updater-fixed-webview2.mjs
+++ scripts/updater-fixed-webview2.mjs
@@ -100,7 +100,7 @@ async function resolveUpdater() {
   Object.entries(updateDataNew.platforms).forEach(([key, value]) => {
     if (value.url) {
       updateDataNew.platforms[key].url =
-        "https://mirror.ghproxy.com/" + value.url;
+        "https://download.clashverge.dev/" + value.url;
     } else {
       console.log(`[Error]: updateDataNew.platforms.${key} is null`);
     }
