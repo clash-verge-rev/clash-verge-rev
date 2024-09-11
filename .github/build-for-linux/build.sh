d8bae11c19335a6d1c6f10137d1930f12b586fa8
oomeow <oomeow@outlook.com>
2024-09-11 21:45:48 +0800

    build: fix linux build

--- .github/build-for-linux/build.sh
+++ .github/build-for-linux/build.sh
@@ -1,6 +1,6 @@
 pnpm install
 pnpm check $INPUT_TARGET --alpha
-sed -i "s/#openssl/openssl={version=\"0.10\",features=[\"vendored\"]}/g" src-tauri/Cargo.toml
+# sed -i "s/#openssl/openssl={version=\"0.10\",features=[\"vendored\"]}/g" src-tauri/Cargo.toml
 
 if [ "$INPUT_TARGET" = "x86_64-unknown-linux-gnu" ]; then
     pnpm build --target $INPUT_TARGET
