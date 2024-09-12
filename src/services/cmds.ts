b888c7729e47e03adae73b376acef6db4db7f8cd
huzibaca <masterorigin91@gmail.com>
2024-09-13 03:21:55 +0800

    refactor: url scheme implementation

--- src/services/cmds.ts
+++ src/services/cmds.ts
@@ -63,6 +63,7 @@ export async function saveProfileFile(index: string, fileData: string) {
 export async function importProfile(url: string) {
   return invoke<void>("import_profile", {
     url,
+    name: null,
     option: { with_proxy: true },
   });
 }
