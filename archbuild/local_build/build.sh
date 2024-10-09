ebead8a99759b63e44c9f403848f644ac8d2a3fb
oomeow <oomeow@outlook.com>
2024-10-10 03:41:25 +0800

    ci: update aur checksums

--- archbuild/local_build/build.sh
+++ archbuild/local_build/build.sh
@@ -8,42 +8,45 @@ CURRENT_SCRIPT_DIR=$(
 PARENT_DIR=$(dirname "$CURRENT_SCRIPT_DIR")
 PROJECT_ROOT_DIR=$(dirname "$PARENT_DIR")
 # get app version
-PROJECT_PACKAGEJSON_PATH="$PROJECT_ROOT_DIR/package.json"
-VERSION=$(cat ${PROJECT_PACKAGEJSON_PATH} | jq '.version' | tr -d '"')
-# deb full name
-DEB_NAME="${APP_NAME}_${VERSION}_amd64.deb"
+VERSION=$(cat $PROJECT_ROOT_DIR/package.json | jq '.version' | tr -d '"')
+# deb file name
+DEB_FILE="${APP_NAME}_${VERSION}_amd64.deb"
 # deb path
-PROJECT_RELEASE_DEB_PATH="$PROJECT_ROOT_DIR/src-tauri/target/release/bundle/deb/${DEB_NAME}"
+DEB_PATH="$PROJECT_ROOT_DIR/src-tauri/target/release/bundle/deb/${DEB_FILE}"
 # arch build bundle name
 ARCH_PKG_NAME=$(grep "^pkgname=" ${CURRENT_SCRIPT_DIR}/PKGBUILD | sed 's/^pkgname=//')
-ARCH_PKG_VERSION=$(grep "^pkgver=" ${CURRENT_SCRIPT_DIR}/PKGBUILD | sed 's/^pkgver=//')
-ARCH_BUNDLE_NAME="${ARCH_PKG_NAME}-${ARCH_PKG_VERSION}-1-x86_64.pkg.tar.zst"
+ARCH_BUNDLE_NAME="${ARCH_PKG_NAME}-${VERSION}-1-x86_64.pkg.tar.zst"
 
 # readonly vars
 readonly CURRENT_SCRIPT_DIR
 readonly PARENT_DIR
 readonly PROJECT_ROOT_DIR
-readonly PROJECT_PACKAGEJSON_PATH
+
 readonly VERSION
-readonly DEB_NAME
-readonly PROJECT_RELEASE_DEB_PATH
+readonly DEB_FILE
+readonly DEB_PATH
+
 readonly ARCH_PKG_NAME
-readonly ARCH_PKG_VERSION
 readonly ARCH_BUNDLE_NAME
 
 # ask for deb build
 read -p "rebuild deb package? (y/n): " rebuild
 if [[ "$rebuild" =~ ^[Yy]$ || -z $rebuild ]]; then
     pnpm build -b deb
-    cp ${PROJECT_RELEASE_DEB_PATH} . || exit 1
+    cp ${DEB_PATH} . || exit 1
 else
-    if [[ -f "./${DEB_NAME}" ]]; then
-        echo -e "\e[33m skip rebuild, use current deb."
+    if [[ -f "./${DEB_FILE}" ]]; then
+        echo -e "\e[33m skip rebuild, use current deb. (${DEB_FILE})"
     else
         echo -e "\e[31m not found deb package, exit."
         exit 0
     fi
 fi
+
+# change version
+sed -i "s/pkgver=.*/pkgver=${VERSION}/" ${CURRENT_SCRIPT_DIR}/PKGBUILD
+# update checksums
+sed -i "s/sha256sums_x86_64=.*/sha256sums_x86_64=(\"$(sha256sum ${DEB_FILE} | awk '{print $1}')\")/" ${CURRENT_SCRIPT_DIR}/PKGBUILD
 
 # starting build arch package
 makepkg -fc
