0e7a0cdeae25a91948070f6f32cbb36d4c69ade5
oomeow <oomeow@outlook.com>
2024-10-15 16:43:21 +0800

    fix: downgrade react devtools

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./src/index.html",
-    "./src/splashscreen.html",
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   darkMode: "class",
   theme: {
     screens: {
--- vite.config.ts
+++ vite.config.ts
@@ -30,89 +30,85 @@ const devtools = () => {
   };
 };
 
-export default defineConfig(({ command }) => {
-  const isDev = command === "serve";
-  return {
-    root: "src",
-    // prevent vite from obscuring rust errors
-    clearScreen: false,
-    // Tauri expects a fixed port, fail if that port is not available
-    server: {
-      port: 5173,
-      strictPort: true,
-    },
-    envPrefix: [
-      "VITE_",
-      "TAURI_PLATFORM",
-      "TAURI_ARCH",
-      "TAURI_FAMILY",
-      "TAURI_PLATFORM_VERSION",
-      "TAURI_PLATFORM_TYPE",
-      "TAURI_DEBUG",
-    ],
-    plugins: [
-      svgr(),
-      react(),
-      legacy({
-        renderLegacyChunks: false,
-        modernTargets: ["edge>=109", "safari>=13"],
-        modernPolyfills: true,
-        additionalModernPolyfills: [
-          "core-js/modules/es.object.has-own.js",
-          "core-js/modules/web.structured-clone.js",
-          path.resolve("./src/polyfills/matchMedia.js"),
-          path.resolve("./src/polyfills/WeakRef.js"),
-          path.resolve("./src/polyfills/RegExp.js"),
-        ],
-      }),
-      monacoEditorPlugin({
-        languageWorkers: ["editorWorkerService", "css", "typescript", "css"],
-        customWorkers: [
-          {
-            label: "yaml",
-            entry: "monaco-yaml/yaml.worker",
-          },
-        ],
-      }),
-      isDev && devtools(),
-    ],
-    build: {
-      outDir: "../dist",
-      emptyOutDir: true,
-      // Tauri uses Chromium on Windows and WebKit on macOS and Linux
-      target:
-        process.env.TAURI_PLATFORM == "windows" ? "chrome105" : "safari13",
-      // don't minify for debug builds
-      minify: !process.env.TAURI_DEBUG ? "esbuild" : false,
-      // 为调试构建生成源代码映射 (sourcemap)
-      sourcemap: !!process.env.TAURI_DEBUG,
-      rollupOptions: {
-        input: {
-          splashscreen: path.resolve(__dirname, "src/splashscreen.html"),
-          main: path.resolve(__dirname, "src/index.html"),
+export default defineConfig({
+  root: "src",
+  // prevent vite from obscuring rust errors
+  clearScreen: false,
+  // Tauri expects a fixed port, fail if that port is not available
+  server: {
+    port: 3000,
+    strictPort: true,
+  },
+  envPrefix: [
+    "VITE_",
+    "TAURI_PLATFORM",
+    "TAURI_ARCH",
+    "TAURI_FAMILY",
+    "TAURI_PLATFORM_VERSION",
+    "TAURI_PLATFORM_TYPE",
+    "TAURI_DEBUG",
+  ],
+  plugins: [
+    svgr(),
+    react(),
+    legacy({
+      renderLegacyChunks: false,
+      modernTargets: ["edge>=109", "safari>=13"],
+      modernPolyfills: true,
+      additionalModernPolyfills: [
+        "core-js/modules/es.object.has-own.js",
+        "core-js/modules/web.structured-clone.js",
+        path.resolve("./src/polyfills/matchMedia.js"),
+        path.resolve("./src/polyfills/WeakRef.js"),
+        path.resolve("./src/polyfills/RegExp.js"),
+      ],
+    }),
+    monacoEditorPlugin({
+      languageWorkers: ["editorWorkerService", "css", "typescript", "css"],
+      customWorkers: [
+        {
+          label: "yaml",
+          entry: "monaco-yaml/yaml.worker",
         },
-        output: {
-          manualChunks(id: string) {
-            if (id.includes("node_modules")) {
-              if (id.includes("monaco-editor")) {
-                return "monaco-editor";
-              }
-              return "vendor";
-            } else if (id.includes("src/components")) {
-              return "verge-components";
+      ],
+    }),
+    !!process.env.TAURI_DEBUG && devtools(),
+  ],
+  build: {
+    outDir: "../dist",
+    emptyOutDir: true,
+    // Tauri uses Chromium on Windows and WebKit on macOS and Linux
+    target: process.env.TAURI_PLATFORM == "windows" ? "chrome105" : "safari13",
+    // don't minify for debug builds
+    minify: !process.env.TAURI_DEBUG ? "esbuild" : false,
+    // 为调试构建生成源代码映射 (sourcemap)
+    sourcemap: !!process.env.TAURI_DEBUG,
+    rollupOptions: {
+      input: {
+        splashscreen: path.resolve(__dirname, "src/splashscreen.html"),
+        main: path.resolve(__dirname, "src/index.html"),
+      },
+      output: {
+        manualChunks(id: string) {
+          if (id.includes("node_modules")) {
+            if (id.includes("monaco-editor")) {
+              return "monaco-editor";
             }
-          },
+            return "vendor";
+          } else if (id.includes("src/components")) {
+            return "verge-components";
+          }
         },
       },
     },
-    resolve: {
-      alias: {
-        "@": path.resolve("./src"),
-        "@root": path.resolve("."),
-      },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve("./src"),
+      "@root": path.resolve("."),
     },
-    define: {
-      OS_PLATFORM: `"${process.platform}"`,
-    },
-  };
+  },
+  define: {
+    OS_PLATFORM: `"${process.platform}"`,
+  },
 });
