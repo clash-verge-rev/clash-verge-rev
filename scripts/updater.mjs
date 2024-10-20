0e8c086729366d7673a584b3142bdc2d825ffbff
oomeow <oomeow@outlook.com>
2024-10-20 16:55:17 +0800

    fix: updater script run failed

--- scripts/updater.mjs
+++ scripts/updater.mjs
@@ -1,10 +1,20 @@
 import { context, getOctokit } from "@actions/github";
 import fetch from "node-fetch";
-import { resolveUpdateLog } from "./updatelog.mjs";
+import path from "path";
 
+const cwd = process.cwd();
+const arg = process.argv.slice(2)[0];
+
+// release version info file
 const UPDATE_TAG_NAME = "updater";
 const UPDATE_JSON_FILE = "update.json";
 const UPDATE_JSON_PROXY = "update-proxy.json";
+
+// log file
+const CHANGE_LOG = "CHANGELOG.txt";
+const UPDATE_LOG = "UPDATELOG.md";
+const update_log_file = path.join(cwd, UPDATE_LOG);
+const change_log_file = path.join(cwd, CHANGE_LOG);
 
 export async function getLatestTag() {
   if (process.env.GITHUB_TOKEN === undefined) {
@@ -33,7 +43,8 @@ export async function getLatestTag() {
 /// upload to update tag's release asset
 async function resolveUpdater() {
   const tag = await getLatestTag();
-
+  const options = { owner: context.repo.owner, repo: context.repo.repo };
+  const github = getOctokit(process.env.GITHUB_TOKEN);
   const { data: latestRelease } = await github.rest.repos.getReleaseByTag({
     ...options,
     tag: tag.name,
@@ -209,4 +220,67 @@ async function getSignature(url) {
   return response.text();
 }
 
-resolveUpdater().catch(console.error);
+// parse the UPDATELOG.md
+export async function resolveUpdateLog(tag) {
+  const reTitle = /^## v[\d\.]+/;
+  const reEnd = /^---/;
+
+  if (!(await fs.pathExists(update_log_file))) {
+    throw new Error("could not found UPDATELOG.md");
+  }
+
+  const data = await fs
+    .readFile(update_log_file)
+    .then((d) => d.toString("utf8"));
+
+  const map = {};
+  let p = "";
+
+  data.split("\n").forEach((line) => {
+    if (reTitle.test(line)) {
+      p = line.slice(3).trim();
+      if (!map[p]) {
+        map[p] = [];
+      } else {
+        throw new Error(`Tag ${p} dup`);
+      }
+    } else if (reEnd.test(line)) {
+      p = "";
+    } else if (p) {
+      map[p].push(line);
+    }
+  });
+
+  if (!map[tag]) {
+    throw new Error(`could not found "${tag}" in UPDATELOG.md`);
+  }
+
+  return map[tag].join("\n").trim();
+}
+
+export async function updateUpdateLog() {
+  const tag = await getLatestTag();
+  const tagTitle = `## v${tag}`;
+  // write all change log content to update log file
+  const changeLogContent = await fs
+    .readFile(change_log_file)
+    .then((d) => d.toString("utf8"));
+  const updateLogContent = await fs
+    .readFile(update_log_file)
+    .then((d) => d.toString("utf8"));
+  if (!updateLogContent.includes(tagTitle)) {
+    const prependContent = `${tagTitle}\n${changeLogContent}\n---\n\n`;
+    const finaleUpdateLogContent = prependContent.concat(updateLogContent);
+    await fs.writeFile(update_log_file, finaleUpdateLogContent);
+    // clean change log file
+    await fs.writeFile(change_log_file, "");
+  } else {
+    console.log(`v${tag} already exists in UPDATELOG.md`);
+  }
+}
+
+if (arg === "--changelog") {
+  updateUpdateLog().catch(console.error);
+} else {
+  resolveUpdater().catch(console.error);
+}
