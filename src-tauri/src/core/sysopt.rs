f0d953ff5921cbd52bfb916ec059c6f0857158eb
huzibaca <masterorigin91@gmail.com>
2024-12-23 06:06:46 +0800

    chore: update default_bypass 1. add 172.29.0.0/16

--- src-tauri/src/core/sysopt.rs
+++ src-tauri/src/core/sysopt.rs
@@ -24,10 +24,11 @@ pub struct Sysopt {
 #[cfg(target_os = "windows")]
 static DEFAULT_BYPASS: &str = "localhost;127.*;192.168.*;10.*;172.16.*;172.17.*;172.18.*;172.19.*;172.20.*;172.21.*;172.22.*;172.23.*;172.24.*;172.25.*;172.26.*;172.27.*;172.28.*;172.29.*;172.30.*;172.31.*;<local>";
 #[cfg(target_os = "linux")]
-static DEFAULT_BYPASS: &str = "localhost,127.0.0.1,192.168.0.0/16,10.0.0.0/8,172.16.0.0/12,::1";
+static DEFAULT_BYPASS: &str =
+    "localhost,127.0.0.1,192.168.0.0/16,10.0.0.0/8,172.16.0.0/12,172.29.0.0/16,::1";
 #[cfg(target_os = "macos")]
 static DEFAULT_BYPASS: &str =
-    "127.0.0.1,192.168.0.0/16,10.0.0.0/8,172.16.0.0/12,localhost,*.local,*.crashlytics.com,<local>";
+    "127.0.0.1,192.168.0.0/16,10.0.0.0/8,172.16.0.0/12,172.29.0.0/16,localhost,*.local,*.crashlytics.com,<local>";
 
 fn get_bypass() -> String {
     let use_default = Config::verge().latest().use_default_bypass.unwrap_or(true);
