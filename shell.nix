ad4a3930b5bca18943b7a95f33d4f5303a483b18
yaner-here <1468275133@qq.com>
2024-12-26 19:41:15 +0800

    chore: copy nix-shell conf from Tauri docs

---
+++ shell.nix
@@ -0,0 +1,27 @@
+let
+  pkgs = import <nixpkgs> { };
+in
+pkgs.mkShell {
+  nativeBuildInputs = with pkgs; [
+    pkg-config
+    gobject-introspection
+    cargo
+    cargo-tauri
+    nodejs
+  ];
+
+  buildInputs = with pkgs;[
+    at-spi2-atk
+    atkmm
+    cairo
+    gdk-pixbuf
+    glib
+    gtk3
+    harfbuzz
+    librsvg
+    libsoup_3
+    pango
+    webkitgtk_4_1
+    openssl
+  ];
+}
