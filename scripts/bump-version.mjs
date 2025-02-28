8f87dfa5cb5f1e354af9f167e2e8a72c66df5273
oomeow <oomeow@outlook.com>
2025-03-01 00:18:22 +0800

    feat: add bump version script

---
+++ scripts/bump-version.mjs
@@ -0,0 +1,46 @@
+import fs from "fs-extra";
+import path from "path";
+
+const cwd = process.cwd();
+let process_argvs = process.argv;
+if (process_argvs.length !== 3) {
+  throw new Error("invalid arguments, please provide a version");
+}
+
+// all version file
+const changeJsonFile = ["package.json", "./src-tauri/tauri.conf.json"];
+const changeFile = [
+  "./src-tauri/Cargo.toml",
+  "./archbuild/alpha/PKGBUILD",
+  "./archbuild/local_build/PKGBUILD",
+  "./archbuild/release/PKGBUILD",
+];
+
+const version = process_argvs[2];
+const versionExp = /^\d+\.\d+\.\d+(-[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*)?$/;
+if (!versionExp.test(version)) {
+  throw new Error("invalid version format");
+}
+
+for (const file of changeJsonFile) {
+  const filePath = path.join(cwd, file);
+  let data = fs.readFileSync(filePath, "utf8");
+  let jsonData = JSON.parse(data);
+  jsonData.version = version;
+  fs.writeFileSync(file, JSON.stringify(jsonData, null, 2));
+}
+
+for (const file of changeFile) {
+  const filePath = path.join(cwd, file);
+  let data = fs.readFileSync(filePath, "utf8");
+  if (data.includes("version = ")) {
+    data = data.replace(/version = ".*"/, `version = "${version}"`);
+  }
+  if (data.includes("pkgver=")) {
+    data = data.replace(
+      /pkgver=\d+\.\d+\.\d+(-[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*)?/,
+      `pkgver=${version}`,
+    );
+  }
+  fs.writeFileSync(file, data);
+}
