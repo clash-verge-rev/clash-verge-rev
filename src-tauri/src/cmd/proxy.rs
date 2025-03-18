23d161c298aa7c33fe0f045c1522db05b13d2ad8
Tunglies <coffee@soe.icu>
2025-03-19 02:04:01 +0800

    refactor: improve proxy retrieval and add window destruction method

--- src-tauri/src/cmd/proxy.rs
+++ src-tauri/src/cmd/proxy.rs
@@ -4,13 +4,21 @@ use crate::module::mihomo::MihomoManager;
 #[tauri::command]
 pub async fn get_proxies() -> CmdResult<serde_json::Value> {
     let mannager = MihomoManager::global();
-    mannager.refresh_proxies().await.unwrap();
-    Ok(mannager.get_proxies())
+    let proxies = mannager
+        .refresh_proxies()
+        .await
+        .map(|_| mannager.get_proxies())
+        .or_else(|_| Ok(mannager.get_proxies()));
+    proxies
 }
 
 #[tauri::command]
 pub async fn get_providers_proxies() -> CmdResult<serde_json::Value> {
     let mannager = MihomoManager::global();
-    mannager.refresh_providers_proxies().await.unwrap();
-    Ok(mannager.get_providers_proxies())
+    let providers = mannager
+        .refresh_providers_proxies()
+        .await
+        .map(|_| mannager.get_providers_proxies())
+        .or_else(|_| Ok(mannager.get_providers_proxies()));
+    providers
 }
