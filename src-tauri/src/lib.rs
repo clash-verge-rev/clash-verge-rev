7383459b9aa7cad10c36897aff25bdf864d99c50
tiegen <litiegen456250@gmail.com>
2025-04-26 18:09:53 +0800

    feat: clean proxy settings before exit (#3404)

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -302,6 +302,13 @@ pub fn run() {
                 api.prevent_exit();
             }
         }
+        tauri::RunEvent::Exit => {
+            // avoid duplicate cleanup
+            if core::handle::Handle::global().is_exiting() {
+                return;
+            }
+            feat::clean();
+        }
         tauri::RunEvent::WindowEvent { label, event, .. } => {
             if label == "main" {
                 match event {
