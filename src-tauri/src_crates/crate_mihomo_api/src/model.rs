59594855b844aeb6c197df98a8c5b49796794d66
Tunglies <tunglies.dev@outlook.com>
2025-06-06 02:40:12 +0800

    chore: add cross-platform cargo check workflow and remove debug features

--- src-tauri/src_crates/crate_mihomo_api/src/model.rs
+++ src-tauri/src_crates/crate_mihomo_api/src/model.rs
@@ -3,17 +3,3 @@ pub struct MihomoManager {
     pub(crate) mihomo_server: String,
     pub(crate) client: reqwest::Client,
 }
-
-#[cfg(feature = "debug")]
-impl Drop for MihomoData {
-    fn drop(&mut self) {
-        println!("Dropping MihomoData");
-    }
-}
-
-#[cfg(feature = "debug")]
-impl Drop for MihomoManager {
-    fn drop(&mut self) {
-        println!("Dropping MihomoManager");
-    }
-}
