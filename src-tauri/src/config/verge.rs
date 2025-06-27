cf437e6d940be410608c776fa5d6ee2ae010e33a
wonfen <eiwonfen@gmail.com>
2025-06-27 09:29:41 +0800

    chore: reduce default auto clean log to 7 days

--- src-tauri/src/config/verge.rs
+++ src-tauri/src/config/verge.rs
@@ -391,7 +391,7 @@ impl IVerge {
             auto_close_connection: Some(true),
             auto_check_update: Some(true),
             enable_builtin_enhanced: Some(true),
-            auto_log_clean: Some(3),
+            auto_log_clean: Some(2),
             webdav_url: None,
             webdav_username: None,
             webdav_password: None,
