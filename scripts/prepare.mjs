de8c54dba2e6857434350f6b7ec3203c73b5edf3
oomeow <oomeow@outlook.com>
2025-08-26 19:38:42 +0800

    chore: ignore install typos on github actions

--- scripts/prepare.mjs
+++ scripts/prepare.mjs
@@ -2,32 +2,35 @@ import { execSync, spawn } from "child_process";
 import { consola } from "consola";
 import ora from "ora";
 
-consola.info("check typos installed");
-const output = execSync("cargo install --list").toString();
-const existsTypos = output.includes("typos-cli");
-if (!existsTypos) {
-  consola.start("Installing typos...");
-  const spinner = ora({
-    text: "Installing typos",
-    color: "yellow",
-    spinner: "circle",
-  });
-  spinner.start();
+const isGithubAction = process.env.GITHUB_TOKEN !== undefined;
+if (!isGithubAction) {
+  consola.info("check typos installed");
+  const output = execSync("cargo install --list").toString();
+  const existsTypos = output.includes("typos-cli");
+  if (!existsTypos) {
+    consola.start("Installing typos...");
+    const spinner = ora({
+      text: "Installing typos",
+      color: "yellow",
+      spinner: "circle",
+    });
+    spinner.start();
 
-  const typos = spawn("cargo", ["install", "typos-cli"]);
-  typos.stdout.on("data", (data) => {
-    spinner.text = data.toString().trim();
-  });
-  typos.stderr.on("data", (data) => {
-    spinner.text = data.toString().trim();
-  });
-  typos.on("close", (code) => {
-    if (code === 0) {
-      spinner.succeed();
-    } else {
-      spinner.fail();
-    }
-  });
-} else {
-  consola.success("typos has installed");
+    const typos = spawn("cargo", ["install", "typos-cli"]);
+    typos.stdout.on("data", (data) => {
+      spinner.text = data.toString().trim();
+    });
+    typos.stderr.on("data", (data) => {
+      spinner.text = data.toString().trim();
+    });
+    typos.on("close", (code) => {
+      if (code === 0) {
+        spinner.succeed();
+      } else {
+        spinner.fail();
+      }
+    });
+  } else {
+    consola.success("typos has installed");
+  }
 }
