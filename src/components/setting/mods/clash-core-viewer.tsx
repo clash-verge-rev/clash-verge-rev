4e29da76019b87bfa04978f7b1b43fccabed361c
oomeow <oomeow@outlook.com>
2025-09-08 10:02:49 +0800

    chore: update core viewer style

--- src/components/setting/mods/clash-core-viewer.tsx
+++ src/components/setting/mods/clash-core-viewer.tsx
@@ -195,9 +195,9 @@ export const ClashCoreViewer = forwardRef<DialogRef, Props>((props, ref) => {
             <ListItemIcon>
               <div className="mx-1 flex w-24 flex-col items-center">
                 <MetaIcon className="h-8 w-8" />
-                <div className="bg-primary-alpha text-primary-main inline-block w-fit rounded-full px-2 py-[2px] text-[10px]">
+                <span className="text-primary-text text-xs">
                   {each.version}
-                </div>
+                </span>
               </div>
             </ListItemIcon>
             <ListItemText
