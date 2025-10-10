a62f43b605d891084cd8ff78e9b652a537f83f6d
Tunglies <77394545+Tunglies@users.noreply.github.com>
2025-10-10 23:18:04 +0800

    chore: update clash_verge_service_ipc to version 2.0.10 and refactor async service manager initialization

--- src-tauri/src/utils/resolve/mod.rs
+++ src-tauri/src/utils/resolve/mod.rs
@@ -46,7 +46,7 @@ pub fn resolve_setup_async() {
             "Version: {}",
             env!("CARGO_PKG_VERSION")
         );
-        init_service_manager().await;
+        futures::join!(init_service_manager());
 
         futures::join!(
             init_work_config(),
