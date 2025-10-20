9f191fea5be61bb87f0d8adc34ad4bc3c09a320f
Slinetrac <realakayuki@gmail.com>
2025-10-20 20:30:59 +0800

    style: cargo fmt

--- src-tauri/src/core/tray/linux.rs
+++ src-tauri/src/core/tray/linux.rs
@@ -4,7 +4,7 @@ use std::{
     time::{Duration, Instant},
 };
 
-use anyhow::{anyhow, Context, Result};
+use anyhow::{Context, Result, anyhow};
 use image::{ImageFormat, ImageReader};
 use ksni::{
     Icon, ToolTip, TrayMethods,
