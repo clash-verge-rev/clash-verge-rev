91e3ec26d699fff4dc6caebe524318c04231a4ac
Tunglies <77394545+Tunglies@users.noreply.github.com>
2025-11-01 22:24:48 +0800

    refactor: remove redundant import of constants in IClashTemp template method

--- src-tauri/src/config/clash.rs
+++ src-tauri/src/config/clash.rs
@@ -1,4 +1,5 @@
 use crate::config::Config;
+use crate::constants::{network, tun as tun_const};
 use crate::utils::dirs::{ipc_path, path_to_str};
 use crate::utils::{dirs, help};
 use crate::{logging, utils::logging::Type};
@@ -48,8 +49,6 @@ impl IClashTemp {
     }
 
     pub fn template() -> Self {
-        use crate::constants::{network, tun as tun_const};
-
         let mut map = Mapping::new();
         let mut tun_config = Mapping::new();
         let mut cors_map = Mapping::new();
