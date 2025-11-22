687530a9cdfc3ad96e14732acd277affceb06a14
Slinetrac <realakayuki@gmail.com>
2025-11-22 19:05:57 +0800

    fix(sysproxy): remove redundant 172.29.0.0/16 entry #4351
    Closes #4351

--- src/components/setting/mods/sysproxy-viewer.tsx
+++ src/components/setting/mods/sysproxy-viewer.tsx
@@ -124,9 +124,9 @@ export const SysproxyViewer = forwardRef<DialogRef>((props, ref) => {
       return "localhost;127.*;192.168.*;10.*;172.16.*;172.17.*;172.18.*;172.19.*;172.20.*;172.21.*;172.22.*;172.23.*;172.24.*;172.25.*;172.26.*;172.27.*;172.28.*;172.29.*;172.30.*;172.31.*;<local>";
     }
     if (getSystem() === "linux") {
-      return "localhost,127.0.0.1,192.168.0.0/16,10.0.0.0/8,172.16.0.0/12,172.29.0.0/16,::1";
+      return "localhost,127.0.0.1,192.168.0.0/16,10.0.0.0/8,172.16.0.0/12,::1";
     }
-    return "127.0.0.1,192.168.0.0/16,10.0.0.0/8,172.16.0.0/12,172.29.0.0/16,localhost,*.local,*.crashlytics.com,<local>";
+    return "127.0.0.1,192.168.0.0/16,10.0.0.0/8,172.16.0.0/12,localhost,*.local,*.crashlytics.com,<local>";
   };
 
   const { data: clashConfig } = useSWR("getClashConfig", getBaseConfig, {
