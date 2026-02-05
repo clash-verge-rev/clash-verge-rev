b441f4b643af27a0fb351dd18128d07bc458cbf8
Tunglies <77394545+Tunglies@users.noreply.github.com>
2026-02-05 15:24:23 +0800

    feat: limit blocking threads to 4, and naming tokio threads

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,5 +1,24 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use std::sync::atomic::{AtomicUsize, Ordering};
+
 fn main() {
+    #[allow(clippy::unwrap_used)]
+    let tokio_runtime = tokio::runtime::Builder::new_multi_thread()
+        // TODO: limit the number of worker threads
+        // .worker_threads(4)
+        .max_blocking_threads(4)
+        .enable_all()
+        .thread_name_fn(|| {
+            static ATOMIC_ID: AtomicUsize = AtomicUsize::new(0);
+            let id = ATOMIC_ID.fetch_add(1, Ordering::SeqCst);
+            format!("clash-verge-runtime-{id}")
+        })
+        .build()
+        .unwrap();
+    let tokio_handle = tokio_runtime.handle();
+    tauri::async_runtime::set(tokio_handle.clone());
+
     #[cfg(feature = "tokio-trace")]
     console_subscriber::init();
 
