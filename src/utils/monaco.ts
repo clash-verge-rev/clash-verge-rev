9989bff4e63ccbd6ba29ffd25af427110b0eca9e
Slinetrac <realakayuki@gmail.com>
2026-03-13 17:33:25 +0800

    refactor(monaco): simplify editor worker bootstrap (#6500)

--- src/utils/monaco.ts
+++ src/utils/monaco.ts
@@ -5,34 +5,26 @@ import cssWorker from "monaco-editor/esm/vs/language/css/css.worker?worker";
 import tsWorker from "monaco-editor/esm/vs/language/typescript/ts.worker?worker";
 import yamlWorker from "monaco-yaml/yaml.worker?worker";
 
-if (typeof self !== "undefined") {
-  const globalScope = self as typeof self & {
-    MonacoEnvironment?: {
-      getWorker: (workerId: string, label: string) => Worker;
-    };
-  };
-
-  globalScope.MonacoEnvironment = {
-    getWorker(_workerId: string, label: string) {
-      switch (label) {
-        case "css":
-        case "less":
-        case "scss":
-          return new cssWorker();
-        case "typescript":
-        case "javascript":
-          return new tsWorker();
-        case "yaml":
-          return new yamlWorker();
-        default:
-          return new editorWorker();
-      }
-    },
-  };
-}
+self.MonacoEnvironment = {
+  getWorker(_, label) {
+    switch (label) {
+      case "css":
+      case "less":
+      case "scss":
+        return new cssWorker();
+      case "typescript":
+      case "javascript":
+        return new tsWorker();
+      case "yaml":
+        return new yamlWorker();
+      default:
+        return new editorWorker();
+    }
+  },
+};
 
 loader.config({ monaco });
 
-void loader.init().catch((error: unknown) => {
+loader.init().catch((error) => {
   console.error("[monaco] Monaco initialization failed:", error);
 });
