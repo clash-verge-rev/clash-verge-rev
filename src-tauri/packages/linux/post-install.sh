041ad5e58898626b56ec4040280f59907809ebf0
Yurii.Huang <yurii.huang@dbappsecurity.com.cn>
2026-03-19 15:02:48 +0800

    fix: deepin-os can't launch app with desktop file

--- src-tauri/packages/linux/post-install.sh
+++ src-tauri/packages/linux/post-install.sh
@@ -2,3 +2,14 @@
 chmod +x /usr/bin/clash-verge-service-install
 chmod +x /usr/bin/clash-verge-service-uninstall
 chmod +x /usr/bin/clash-verge-service
+
+. /etc/os-release
+
+if [ $ID == "deepin" ]; then
+    PACKAGE_NAME="$DPKG_MAINTSCRIPT_PACKAGE"
+    echo "Fixing deepin desktop files"
+    for f in $(dpkg -L "$PACKAGE_NAME" |grep ".desktop")
+    do
+        sed -i "s/MimeType=x-scheme-handler/clash;//g" "$f"
+    done
+fi
