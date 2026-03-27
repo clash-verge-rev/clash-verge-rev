bce3be701e83a2956b6f99000373623589bbeabc
wysha-object <152023695+wysha-object@users.noreply.github.com>
2026-03-27 15:44:11 +0800

    Update documentation to English

--- src-tauri/src/feat/proxy.rs
+++ src-tauri/src/feat/proxy.rs
@@ -36,7 +36,7 @@ pub async fn toggle_system_proxy() {
 }
 
 /// Toggle TUN mode on/off
-/// @return 切换后的开关状态
+/// Returns the updated toggle state
 pub async fn toggle_tun_mode(not_save_file: Option<bool>) -> bool {
     let enable = Config::verge().await.latest_arc().enable_tun_mode;
     let mut enable = enable.unwrap_or(false);
