6d1803d99cd636a75f2afc23690587894c9ea6e3
Tunglies <77394545+Tunglies@users.noreply.github.com>
2026-05-25 19:35:17 +0800

    refactor(enhance): move values while sorting fields

--- src-tauri/src/enhance/field.rs
+++ src-tauri/src/enhance/field.rs
@@ -1,6 +1,5 @@
 use serde_yaml_ng::{Mapping, Value};
 use smartstring::alias::String;
-use std::collections::HashSet;
 
 pub const HANDLE_FIELDS: [&str; 12] = [
     "mode",
@@ -32,29 +31,30 @@ pub fn use_lowercase(config: &Mapping) -> Mapping {
     ret
 }
 
-pub fn use_sort(config: Mapping) -> Mapping {
+pub fn use_sort(mut config: Mapping) -> Mapping {
     let mut ret = Mapping::new();
     HANDLE_FIELDS.into_iter().for_each(|key| {
         let key = Value::from(key);
-        if let Some(value) = config.get(&key) {
-            ret.insert(key, value.clone());
+        if let Some(value) = config.remove(&key) {
+            ret.insert(key, value);
         }
     });
 
-    let supported_keys: HashSet<&str> = HANDLE_FIELDS.into_iter().chain(DEFAULT_FIELDS).collect();
-
-    let config_keys: HashSet<&str> = config.keys().filter_map(|e| e.as_str()).collect();
-
-    config_keys.difference(&supported_keys).for_each(|&key| {
-        let key = Value::from(key);
-        if let Some(value) = config.get(&key) {
-            ret.insert(key, value.clone());
+    let mut default_fields = Mapping::new();
+    for (key, value) in config {
+        if let Some(key_str) = key.as_str() {
+            if DEFAULT_FIELDS.contains(&key_str) {
+                default_fields.insert(key, value);
+            } else if !HANDLE_FIELDS.contains(&key_str) {
+                ret.insert(key, value);
+            }
         }
-    });
+    }
+
     DEFAULT_FIELDS.into_iter().for_each(|key| {
         let key = Value::from(key);
-        if let Some(value) = config.get(&key) {
-            ret.insert(key, value.clone());
+        if let Some(value) = default_fields.remove(&key) {
+            ret.insert(key, value);
         }
     });
 
