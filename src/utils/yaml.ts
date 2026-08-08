6d5d174a35a81dcba8a1047122ef13b038198c43
wysha-object <starssail@outlook.com>
2026-08-09 00:06:05 +0800

    fix

--- src/utils/yaml.ts
+++ src/utils/yaml.ts
@@ -1,5 +1,7 @@
 import { load, LoadOptions } from 'js-yaml'
 
+import { debugLog } from './debug'
+
 /**
  * 解析失败返回null, 不抛出错误
  */
@@ -7,7 +9,8 @@ export function parseYamlSafe(input: string, options?: LoadOptions) {
   try {
     const rs = load(input, options)
     return rs ?? null
-  } catch (ignore) {
+  } catch (e) {
+    debugLog('parseYamlSafe failed', e)
     return null
   }
 }
